e957be8512f28d58dbdd765f63eab0f1bccbefbd
Abasagha70 <158489212+Abasagha70@users.noreply.github.com>
2024-02-12 02:42:03 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            progressBar1 = new ProgressBar();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            label4 = new Label();
+            btn_addProgressBar = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.ForeColor = SystemColors.InfoText;
+            label1.Location = new Point(251, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(258, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Zəhmət Olmasa Məhsulları əlavə edin";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(139, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Məhsulun adı";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(139, 273);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 28);
+            button1.TabIndex = 2;
+            button1.Text = "Əlavə et";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(139, 162);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Qiyməti";
+            label3.Click += label3_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(811, 232);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(67, 226);
+            progressBar1.TabIndex = 4;
+            progressBar1.Click += progressBar1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(139, 107);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(139, 198);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 6;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(455, 108);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(150, 27);
+            numericUpDown1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(455, 71);
+            label4.Name = "label4";
+            label4.Size = new Size(36, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Sayı";
+            // 
+            // btn_addProgressBar
+            // 
+            btn_addProgressBar.Location = new Point(455, 153);
+            btn_addProgressBar.Name = "btn_addProgressBar";
+            btn_addProgressBar.Size = new Size(109, 29);
+            btn_addProgressBar.TabIndex = 9;
+            btn_addProgressBar.Text = "Sayı Əlavə et";
+            btn_addProgressBar.UseVisualStyleBackColor = true;
+            btn_addProgressBar.Click += btn_addProgressBar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(890, 470);
+            Controls.Add(btn_addProgressBar);
+            Controls.Add(label4);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(progressBar1);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Label label3;
+        private ProgressBar progressBar1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private NumericUpDown numericUpDown1;
+        private Label label4;
+        private Button btn_addProgressBar;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿namespace task1
+{
+    public partial class Form1 : Form
+    {
+       
+   
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Məhsulunuz əlavə olundu");
+        }
+
+      
+
+        private void Btn_addProgressBar_Click(object sender, EventArgs e)
+        {
+            var progressBarValue = progressBar1.Value;
+            progressBarValue += Convert.ToInt32(numericUpDown1.Value);
+            if (progressBarValue <= 10)
+                progressBar1.Value= progressBarValue;
+        }
+    }
+    }
+
